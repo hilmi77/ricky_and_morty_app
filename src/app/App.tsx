e148bca1f5b6a822c97f53827d43611d9b1b7177
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-02-29 13:09:49 +0300

    Add routing and new pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Favorites from "../pages/favorites/Favorites";
+import LocationList from "../pages/locationsList/LocationList";
+import CharacterDetails from "../pages/characterDetails/CharacterDetails";
+import LocationsCharacters from "../pages/locationCharacters/LocationsCharacters";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/:locationId" element={<LocationsCharacters />} />
+        <Route
+          path="/:locationId/:characterId"
+          element={<CharacterDetails />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/locationCharacters/LocationsCharacters.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LocationsCharacters = () => {
+  return <div>LocationsCharacters</div>;
+};
+
+export default LocationsCharacters;
