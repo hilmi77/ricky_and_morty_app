ccac9d6459a5f54d1ceb655ca0fa9df20dcde93c
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-02-29 19:11:00 +0300

    Update location list layout and add pagination

--- src/pages/locationsList/LocationList.tsx
+++ src/pages/locationsList/LocationList.tsx
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from "react";
 import { fetchLocations } from "../../service/axios";
 import LocationCard from "../../components/locationCard/LocationCard";
 import "./LocationList.scss";
+import Pagination from "../../components/pagination/Pagination";
 
 const LocationList = () => {
   const [locations, setLocations] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [itemsPerPage] = useState<number>(6);
   useEffect(() => {
     const getLocations = async () => {
       const data = await fetchLocations();
@@ -16,22 +18,35 @@ const LocationList = () => {
 
     getLocations();
   }, []);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = locations.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(locations.length / itemsPerPage);
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   return (
-    <div className="container">
-      <h1>Locations</h1>
-      <div className="locations_list">
-        {locations.map((location: any) => (
-          <LocationCard
-            key={location.id}
-            id={location.id}
-            name={location.name}
-            type={location.type}
-            dimension={location.dimension}
-            residents={location.residents}
-          />
-        ))}
+    <>
+      <div className="container">
+        <h1>Locations</h1>
+        <div className="locations_list">
+          {currentItems.map((location: any) => (
+            <LocationCard
+              key={location.id}
+              id={location.id}
+              name={location.name}
+              type={location.type}
+              dimension={location.dimension}
+              residents={location.residents}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={paginate}
+      />
+    </>
   );
 };
 
