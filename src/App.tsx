74af3b8c20ccd36d433335db50c01ea9234a6e38
hilmi <hilmi.kale@ogr.iu.edu.tr>
2024-03-01 12:06:33 +0300

    Remove unused import statement in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Favorites from "./pages/favorites/Favorites";
 import LocationList from "./pages/locationsList/LocationList";
